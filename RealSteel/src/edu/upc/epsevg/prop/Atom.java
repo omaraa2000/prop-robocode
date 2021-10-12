46c256b385a7a26b291d3d79f0a6a19e070bfaf4
Omara <omaraaibar2000@gmail.com>
2021-10-12 12:03:13 +0200

    Atom dispara
    enfocando el disparo

--- RealSteel/src/edu/upc/epsevg/prop/Atom.java
+++ RealSteel/src/edu/upc/epsevg/prop/Atom.java
@@ -19,22 +19,28 @@ import robocode.ScannedRobotEvent;
 public class Atom extends AdvancedRobot {
 
     private byte scanDirection = 1;
+    private double RadarDirection;
+    private double GunDirection;
 
     public void run() {
         setTurnLeft(getHeading());
         setAdjustGunForRobotTurn (true);       //activamos el cañon del robot
         setAdjustRadarForRobotTurn(true);      //activamos el radar del robot
         while(true) {
-            setAhead(1000);                    //hacer que se mueva
-            setTurnRadarRight (360);      //girar el radar en sentido positivo
+            setAhead(1000);                    //hacer que se mueva hacia adelante
+            setTurnRadarRight (360);        //girar el radar 360
             execute();
         }
     }
    
     public void onScannedRobot(ScannedRobotEvent event) {
-        scanDirection *= -1;
-        setTurnRadarRight(1000 * scanDirection);
-        //fire(Rules.MAX_BULLET_POWER);
+        //scanDirection *= -1;
+        //setTurnRadarRight(1000 * scanDirection);
+        RadarDirection = (getHeading() - getRadarHeading() + event.getBearing());
+        setTurnRadarRight(RadarDirection); //hacemos la diferencia entre el rumbo de nuestro tanque ( getHeading () ) y el rumbo de nuestro radar ( getRadarHeading () ) y agregamos el rumbo al robot escaneado ( event.getBearing () ) 
+        GunDirection = RadarDirection * (math.PI/180);
+        getGunHeadingRadians(GunDirection);
+        fire(Rules.MAX_BULLET_POWER);
         execute();
     }
     public void onHitByBullet(HitByBulletEvent event) {
